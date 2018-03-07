c69bc3c82157519a802707032fe8cbed95e92435
mikk5394 <31307509+mikk5394@users.noreply.github.com>
2018-03-07 23:06:54 +0100

    Add files via upload

---
+++ run.java
@@ -0,0 +1,11 @@
+public class run {
+
+    public static void main(String[] args) {
+
+        MonsterTruck mymonstertruck = new MonsterTruck();
+
+        mymonstertruck.m1();
+        mymonstertruck.m2();
+        System.out.println(mymonstertruck);
+    }
+}
